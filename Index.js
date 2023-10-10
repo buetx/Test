b4c3a1ab74b78d4870ce2f1088e271860aed6a71
mcubd <106980362+mcubd@users.noreply.github.com>
2023-10-11 01:39:22 +0600

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,8 +1,35 @@
-var express = require('express');
-var app = express();
-app.get('/links', function (req, res) {
-res.send("Welocme to GeeksforGeeks!");
+const express = require("express");
+const cors = require("cors");
+const  fetch =require('node-fetch')
+const bodyParser=require('body-parser') 
+
+
+//let chrome = {};
+//let puppeteer;
+
+
+//if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+ // chrome = require("chrome-aws-lambda");
+  //puppeteer = require("puppeteer-core");
+//} else {
+ // puppeteer = require("puppeteer");
+// }
+
+const app = express();
+// const PORT = 3000;
+
+app.listen(process.env.PORT || 8000, () => {    console.log(`Server is running on PORT: ${"PORT"}`);
 });
 
-app.listen(process.env.PORT || 8000, () => {    console.log(`Server is running on PORT: `);
-});
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH']
+}));
+// app.set('trust proxy', true)
+// app.use(express.json())
+app.use(bodyParser.text({ type: "*/*" }));
+
+
+app.get('/links',async (req,res)=>{
+  res.send('888jj')
+})
